01383615a5f2ce4a498d54b406a83012032c3301
Vlad Tihomirov <7292963@mail.ru>
2025-09-29 15:11:36 +0300

    add something

---
+++ JavaCore/src/main/java/Exception/EndTask/File.java
@@ -0,0 +1,4 @@
+package Exception.EndTask;
+
+public class File {
+}
--- JavaCore/src/main/java/Exception/EndTask/Main.java
+++ JavaCore/src/main/java/Exception/EndTask/Main.java
@@ -2,6 +2,6 @@ package Exception.EndTask;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(1);
+        System.out.println(123);
     }
 }
